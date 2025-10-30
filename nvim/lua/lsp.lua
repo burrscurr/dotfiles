0926695085a0fe99c7593d9d36b9e071b90fe88a
burrscurr <burrscurr@users.noreply.github.com>
2025-10-30 23:48:25 +0100

    Revert to default mappings where available

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -3,21 +3,10 @@
 local bufopts = { noremap=true, silent=true, buffer=bufnr }
 vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
 vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
 vim.keymap.set('n', 'K', function() vim.lsp.buf.hover({ border = "rounded" }) end, bufopts)
 vim.keymap.set('n', '<C-k>', function() vim.lsp.buf.signature_help({ border = "rounded", focusable = false }) end, bufopts)
-vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-vim.keymap.set('n', '<space>wl', function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, bufopts)
-vim.keymap.set('n', '<space>r', vim.lsp.buf.rename, bufopts)
-vim.keymap.set('n', '<space>c', vim.lsp.buf.code_action, bufopts)
 vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
-vim.keymap.set('n', '<space>t', vim.lsp.buf.type_definition, bufopts)
 vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, bufopts)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, bufopts)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, bufopts)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, bufopts)
 
 -- Use CTRL-space to trigger LSP completion.
 vim.keymap.set('i', '<c-space>', function()
